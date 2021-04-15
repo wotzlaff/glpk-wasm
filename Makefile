bb2f1cad8ec575c7babb3bc57d2a63fd3d487e93
Nico Strasdat <Nico.Strasdat@tu-dresden.de>
2021-04-15 16:21:14 +0200

    Provide subset of functions

--- Makefile
+++ Makefile
@@ -6,22 +6,28 @@ EXTRA_EXPORTED_RUNTIME_METHODS = EXTRA_EXPORTED_RUNTIME_METHODS=[$(subst $(space
 
 all: prepare extract build
 
+parts = min graph mip more full all
+
+build: $(patsubst %,build-%,$(parts))
+
 prepare:
 	mkdir -p dist
 
 extract:
 	clang -Xclang -ast-dump=json -fsyntax-only $(GLPK_DIR)/src/glpk.h | node scripts/extract_functions.js
 
-build:
+parts-min = problem simplex
+parts-graph = graph-basic graph-optimization
+parts-mip = $(parts-min) mip ios
+parts-more = $(parts-mip) rw-data interior advanced-tableau
+parts-full = $(parts-more) advanced-analysis advanced-basis basis env index rw-mathprog rw-solution scaling
+parts-all = $(parts-full) npp undoc $(parts-graph) cnf
+
+build-%:
 	emcc -O3 \
 		-s $(EXTRA_EXPORTED_RUNTIME_METHODS) \
-		-s EXPORTED_FUNCTIONS=[$(subst $(space),$(comma),$(patsubst %,'_%',$(filter-out #%,$(file < exported-functions.txt)) free))] \
+		-s EXPORTED_FUNCTIONS=[$(subst $(space),$(comma),$(patsubst %,'_%',$(filter-out #%,$(foreach part,$(parts-$*),$(file < parts/$(part).txt))) free))] \
 		-s ALLOW_MEMORY_GROWTH=1 \
 		-s MODULARIZE=1 \
-		-o dist/libglpk.js \
+		-o dist/glpk.$*.js \
 		$(GLPK_DIR)/src/.libs/libglpk.so
-
-dist/index.js:
-	@echo "(function (root, factory) {if (typeof define === 'function' && define.amd) {define([], factory)} else if (typeof module === 'object' && module.exports) {module.exports = factory()} else {root.streamingPercentiles = factory()}}(typeof self !== 'undefined' ? self : this, function () {\n" > $@
-	@cat dist/libglpk.js >> $@
-	@echo "\nreturn Module}))" >> $@
--- scripts/extract_functions.js
+++ scripts/extract_functions.js
@@ -1,90 +1,92 @@
-const fs = require('fs')
-const data = JSON.parse(fs.readFileSync(0, 'utf-8'))
+const fs = require("fs");
+const data = JSON.parse(fs.readFileSync(0, "utf-8"));
 
-const ptrTypes = new Set(['TermCallback', 'ErrorCallback'])
+const ptrTypes = new Set(["TermCallback", "ErrorCallback"]);
 
 function formatType(t) {
   const res = {
-    int: 'number',
-    void: 'void',
-    double: 'number',
-    size_t: 'number',
-    char: 'Char',
-    glp_smcp: 'SMCP',
-    glp_prob: 'Problem',
-    glp_iptcp: 'IPTCP',
-    glp_iocp: 'IOCP',
-    glp_bfcp: 'BFCP',
-    glp_mpscp: 'MPSCP',
-    glp_cpxcp: 'CPXCP',
-    // glp_graph: 'Graph',
-    // glp_arc: 'Arc',
-    glp_tree: 'Tree',
-    glp_prep: 'Prep',
-    glp_attr: 'Attr',
-    glp_tran: 'Translator',
-    'void (*)(void *)': 'ErrorCallback',
-    'int (*)(void *, const char *)': 'TermCallback',
-  }[t]
+    int: "number",
+    void: "void",
+    double: "number",
+    size_t: "number",
+    char: "Char",
+    glp_smcp: "SMCP",
+    glp_prob: "Problem",
+    glp_iptcp: "IPTCP",
+    glp_iocp: "IOCP",
+    glp_bfcp: "BFCP",
+    glp_mpscp: "MPSCP",
+    glp_cpxcp: "CPXCP",
+    glp_graph: "Graph",
+    glp_arc: "Arc",
+    glp_tree: "Tree",
+    glp_prep: "Prep",
+    glp_attr: "Attr",
+    glp_tran: "Translator",
+    "void (*)(void *)": "ErrorCallback",
+    "int (*)(void *, const char *)": "TermCallback",
+  }[t];
 
   if (res !== undefined) {
-    return res
+    return res;
   }
 
-  if (t.startsWith('const ')) {
-    return formatType(t.substr(6))
+  if (t.startsWith("const ")) {
+    return formatType(t.substr(6));
   }
-  if (t.endsWith(' *')) {
-    const res = formatType(t.substr(0, t.length - 2)) + 'Ptr'
-    ptrTypes.add(res)
-    return res
+  if (t.endsWith(" *")) {
+    const res = formatType(t.substr(0, t.length - 2)) + "Ptr";
+    ptrTypes.add(res);
+    return res;
   }
-  throw new Error(`unknown type: ${t}`)
+  throw new Error(`unknown type: ${t}`);
 }
 
 function getReturnType(t) {
-  const idx = t.indexOf('(')
-  return t.substr(0, idx).trim()
+  const idx = t.indexOf("(");
+  return t.substr(0, idx).trim();
 }
 
 function filter(method) {
-  return ['glp_netgen_prob'].indexOf(method) < 0
+  return ["glp_netgen_prob"].indexOf(method) < 0;
 }
 
-const signatures = []
-const functions = []
+const signatures = [];
+const functions = [];
 
 data.inner
-  .filter(el => el.kind === 'FunctionDecl')
-  .filter(el => filter(el.name))
-  .forEach(el => {
+  .filter((el) => el.kind === "FunctionDecl")
+  .filter((el) => filter(el.name))
+  .forEach((el) => {
     try {
-      const retType = formatType(getReturnType(el.type.qualType))
+      const retType = formatType(getReturnType(el.type.qualType));
       const args = el.inner
-        ? el.inner.map(arg => `${arg.name}: ${formatType(arg.type.qualType)}`).join(', ')
-        : ''
-      signatures.push(`_${el.name}(${args}): ${retType}`)
-      functions.push(el.name)
+        ? el.inner
+            .map((arg) => `${arg.name}: ${formatType(arg.type.qualType)}`)
+            .join(", ")
+        : "";
+      signatures.push(`_${el.name}(${args}): ${retType}`);
+      functions.push(el.name);
     } catch (err) {
       // signatures.push('// FAIL: ' + el.name)
-      functions.push('#' + el.name)
+      functions.push("#" + el.name);
     }
-  })
+  });
 
-fs.writeFileSync('./exported-functions.txt', functions.join('\n'))
+fs.writeFileSync("./exported-functions.txt", functions.join("\n"));
 
 fs.writeFileSync(
-  './dist/index.d.ts',
+  "./dist/index.d.ts",
   [
     '///<reference path="./emscripten.d.ts"/>',
-    '',
-    ...Array.from(ptrTypes, t => `export declare class ${t} extends Ptr {}`),
-    '',
-    'export declare interface GLPKModule extends Module {',
-    ...signatures.map(s => '  ' + s),
-    '}',
-    '',
-    'export default function initialize(): Promise<GLPKModule>',
-    '',
-  ].join('\n')
-)
+    "",
+    ...Array.from(ptrTypes, (t) => `export declare class ${t} extends Ptr {}`),
+    "",
+    "export declare interface GLPKModule extends Module {",
+    ...signatures.map((s) => "  " + s),
+    "}",
+    "",
+    "export default function initialize(): Promise<GLPKModule>",
+    "",
+  ].join("\n")
+);
